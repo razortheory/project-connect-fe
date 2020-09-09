6999a37dc3bcea85e1a92ae75cf1f5fec8da61ab
doasync <doasync@gmail.com>
2020-09-09 17:26:06 +0300

    Search countries

--- src/ui/components/search-results.tsx
+++ src/ui/components/search-results.tsx
@@ -1,19 +1,26 @@
 import { useStore } from 'effector-react';
 import React from 'react';
 
-import { $noSearchResults } from './search';
+import { mapCountry } from '~/core/routes';
+import { CountryData } from '~/features/map/types';
+import { Link } from '~/lib/router';
+
+import { $noSearchResults, $searchResults } from './search';
 
 export const NotFound = () => <div>Not found</div>;
 
+export const CountryList = () => (
+  <ul className="list">
+    {useStore($searchResults)?.map((country: CountryData) => (
+      <li key={country.id} className="list__item list__item--not-verified">
+        <Link to={mapCountry} params={{ id: country.id }}>
+          {country.name}
+        </Link>
+      </li>
+    ))}
+  </ul>
+);
+
 export const SearchResults = () => (
-  <>
-    {useStore($noSearchResults) ? (
-      <NotFound />
-    ) : (
-      <ul className="list">
-        <li className="list__item list__item--not-verified">Georgia</li>
-        <li className="list__item list__item--not-verified">Germany</li>
-      </ul>
-    )}
-  </>
+  <>{useStore($noSearchResults) ? <NotFound /> : <CountryList />}</>
 );
--- src/ui/components/search.tsx
+++ src/ui/components/search.tsx
@@ -2,7 +2,9 @@ import { createEvent, createStore, sample } from 'effector';
 import { useStore } from 'effector-react';
 import React, { ChangeEvent } from 'react';
 
-import { setPayload } from '~/lib/effector-kit';
+import { $countriesData } from '~/features/map/model';
+import { CountryData } from '~/features/map/types';
+import { getVoid, setPayload } from '~/lib/effector-kit';
 
 // Helpers
 export const getTargetValue = (event: ChangeEvent<HTMLInputElement>) =>
@@ -10,13 +12,28 @@ export const getTargetValue = (event: ChangeEvent<HTMLInputElement>) =>
 
 // Model
 const changeSearchText = createEvent<string>();
+const clearSearchText = createEvent();
 
 const $searchText = createStore('');
+export const $searchResults = createStore<CountryData[] | null>([]);
 export const $showSearchResults = createStore(false);
 export const $noSearchResults = createStore(false);
 
 // Init
 $searchText.on(changeSearchText, setPayload);
+$searchText.reset(clearSearchText);
+
+sample({
+  source: $countriesData,
+  clock: $searchText,
+  fn: (countriesData: CountryData[] | null, searchText: string) =>
+    countriesData?.filter((countryData) =>
+      countryData.name
+        .toLocaleLowerCase()
+        .includes(searchText.toLocaleLowerCase())
+    ) ?? null,
+  target: $searchResults,
+});
 
 sample({
   source: $searchText,
@@ -25,13 +42,14 @@ sample({
 });
 
 sample({
-  source: $searchText,
-  fn: (searchText) => searchText.includes('x'),
+  source: $searchResults,
+  fn: (countriesFound) => !countriesFound?.length,
   target: $noSearchResults,
 });
 
 // View
 const onChange = changeSearchText.prepend(getTargetValue);
+const onClear = clearSearchText.prepend(getVoid);
 
 export const Search = () => (
   <div className="sidebar__search-bar search-bar">
@@ -43,7 +61,7 @@ export const Search = () => (
       onChange={onChange}
       value={useStore($searchText)}
     />
-    <button className="search-bar__close" type="button">
+    <button className="search-bar__close" type="button" onClick={onClear}>
       +
     </button>
   </div>
