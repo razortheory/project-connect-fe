83fe1673a38e53c2bbaba85c4fe0887f1ecce374
Egor Lukashev <egor.l@razortheory.com>
2020-09-21 18:32:37 +0300

    Added sort select to the country progress dashboard

--- src/@/project/@/dashboard/model.ts
+++ src/@/project/@/dashboard/model.ts
@@ -3,12 +3,16 @@ import { ChangeEvent } from 'react';
 
 import { CountryMetaData } from '~/api/types';
 
+import { SortValues } from '@/map/@/sidebar/types';
+import { defaultSortValue } from '@/project/@/dashboard/constats';
+
 export const getTargetValue = (event: ChangeEvent<HTMLInputElement>): string =>
   event.target.value;
 
 export const changeSearchText = createEvent<string>();
 export const changeViewType = createEvent();
 export const clearSearchText = createEvent();
+export const changeSortValue = createEvent<SortValues>();
 
 export const $searchText = createStore('');
 export const $hasSearchText = createStore(false);
@@ -16,3 +20,4 @@ export const $countries = createStore<CountryMetaData[]>([]);
 export const $notFound = createStore(false);
 export const $isListType = createStore(false);
 export const $isLoading = createStore<boolean>(false);
+export const $sortValue = createStore<SortValues>(defaultSortValue);
