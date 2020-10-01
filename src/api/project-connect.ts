a15a1ddef2cf29e8e7d34206e0859b4e19c1d657
doasync <doasync@gmail.com>
2020-10-01 14:29:29 +0300

    Change url scheme to https

--- src/api/project-connect.ts
+++ src/api/project-connect.ts
@@ -18,7 +18,7 @@ import {
 } from './types';
 
 export const request = createRequest({
-  baseUrl: 'http://proco-lb-test.th13f.live/',
+  baseUrl: 'https://proco-lb-test.th13f.live/',
 });
 
 export const fetchCountryFx = createEffect(
